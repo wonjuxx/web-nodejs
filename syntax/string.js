a7ce9e9588fa7eacbdb3e6ae3201ae448123cca7
Wonju Jeong <73467408+wonjuxx@users.noreply.github.com>
2020-11-09 13:05:11 +0900

    first version

---
+++ syntax/string.js
@@ -0,0 +1,1 @@
+console.log('Definition and Usage. The length property returns the length of a string (number of characters). The length of an empty string is 0.'.length);
---
+++ syntax/variable2.js
@@ -0,0 +1,3 @@
+var name = 'k8805';
+var letter = 'Dear '+name+' Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore '+name+' magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit '+name+' in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. '+name;
+console.log(letter);
